4c86f81f8d21c7f3766b9459fb288efa2bbb57b6
Zdiggidy26 <78041840+Zdiggidy26@users.noreply.github.com>
2021-02-24 11:54:42 -0700

    my project

---
+++ app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+use App\Models\Task;
+class User extends Authenticatable
+{
+
+    public function tasks()
+    {
+    	return $this->hasMany(Task::class);
+    }
+
+	  
+}
+
